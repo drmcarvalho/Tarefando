ac088dd226661830110e3c5a8cc25db8d7df4272
drmcarvalho <furiano.k@gmail.com>
2025-09-22 21:03:27 -0300

    allow cors

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Tarefando.Api.Business;
 using Tarefando.Api.Database;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,14 @@ builder.Services.AddMemoryCache();
 builder.Services.AddBusiness();
 builder.Services.AddDatabase();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +31,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
